ab52781a16672958d02643cd348461e6bcd51fc4
merezz-code <ezzouhairimeryem@gmail.com>
2026-01-19 12:20:00 +0100

    'finalize'

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -836,9 +836,6 @@ export default function CourseDetail() {
                 ))}
               </div>
 
-              {/* 🏆 QUIZ */}
-             
-
               {loadingQuizzes && (
                 <div className="mt-12 flex items-center justify-center">
                   <Loader2 className="w-8 h-8 animate-spin text-primary" />
